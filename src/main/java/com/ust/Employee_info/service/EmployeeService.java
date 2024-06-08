c2b3b74f17d3d32307de68044051b36fb08b0743
Shirisha-1313 <106987221+Shirisha-1313@users.noreply.github.com>
2024-06-09 01:33:14 +0530

    Update EmployeeService.java

--- src/main/java/com/ust/Employee_info/service/EmployeeService.java
+++ src/main/java/com/ust/Employee_info/service/EmployeeService.java
@@ -1,28 +1,42 @@
 package com.ust.Employee_info.service;
 
 import com.ust.Employee_info.model.Employee;
+import com.ust.Employee_info.exception.ResourceNotFoundException;
 import com.ust.Employee_info.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
-
 @Service
 public class EmployeeService {
+
     @Autowired
     private EmployeeRepository employeeRepository;
+
+    public Employee saveEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
 
     public List<Employee> getAllEmployees() {
         return employeeRepository.findAll();
     }
 
-    public Optional<Employee> getEmployeeById(Long id) {
-        return employeeRepository.findById(id);
+    public Optional<Employee> getEmployeeById(int empid) {
+        return employeeRepository.findById(empid);
     }
 
-    public Employee addEmployee(Employee employee) {
+    public Employee updateEmployee(int empid, Employee employeeDetails) {
+        Employee employee = employeeRepository.findById(empid).orElseThrow(() -> new ResourceNotFoundException("Employee not found"));
+        employee.setName(employeeDetails.getName());
+        employee.setBasicSalary(employeeDetails.getBasicSalary());
+        employee.setGrade(employeeDetails.getGrade());
         return employeeRepository.save(employee);
     }
 
+    public void deleteEmployee(int empid) {
+        Employee employee = employeeRepository.findById(empid).orElseThrow(() -> new ResourceNotFoundException("Employee not found"));
+        employeeRepository.delete(employee);
+    }
 }
+
