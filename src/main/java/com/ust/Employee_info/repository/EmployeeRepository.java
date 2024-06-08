7f2835f94793b4d47a7c56abbc944b412ec93873
Shirisha-1313 <106987221+Shirisha-1313@users.noreply.github.com>
2024-06-09 01:32:34 +0530

    Update EmployeeRepository.java

--- src/main/java/com/ust/Employee_info/repository/EmployeeRepository.java
+++ src/main/java/com/ust/Employee_info/repository/EmployeeRepository.java
@@ -4,5 +4,9 @@ package com.ust.Employee_info.repository;
 import com.ust.Employee_info.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
