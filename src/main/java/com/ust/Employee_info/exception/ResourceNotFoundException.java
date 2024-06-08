eff1e5794d8b0924c243f6dd9d88566e3f3f5e4d
Shirisha-1313 <106987221+Shirisha-1313@users.noreply.github.com>
2024-06-09 01:35:09 +0530

    Update and rename EmployeeNotFoundException.java to ResourceNotFoundException.java

---
+++ src/main/java/com/ust/Employee_info/exception/ResourceNotFoundException.java
@@ -0,0 +1,16 @@
+package com.ust.Employee_info.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
+
+
+
+
